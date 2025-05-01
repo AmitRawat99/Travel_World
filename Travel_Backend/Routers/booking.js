10efc4a31a9e37916a70bab46e6eeaa545734e7a
Amit Rawat <167463184+AmitRawat99@users.noreply.github.com>
2025-05-01 21:24:01 +0530

    first commit

---
+++ Travel_Backend/Routers/booking.js
@@ -0,0 +1,11 @@
+
+import express from 'express'
+const router = express.Router()
+import { VerifyAdmin , VerfyToken , Verifyuser } from '../Utils/verfyToken.js'
+import { createBooking , getAllBooking , getBooking } from '../controllers/bookingController.js'
+
+router.post('/' , VerifyAdmin , createBooking)
+router.get('/:id' , VerifyAdmin , getBooking)
+router.get('/' , VerifyAdmin , getAllBooking)
+
+export default router
